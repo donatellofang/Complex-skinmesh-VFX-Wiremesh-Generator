207a901a6b8f5f0bf4c555d8c41021783eefc856
donatellofang <donatellofang@gmail.com>
2022-07-16 15:37:47 +0800

    first commit

---
+++ Assets/Tools/Scripts/Editor/GenerateWiremesh.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+
+
+public class GenerateWiremesh : EditorWindow
+{
+    GameObject currentSelectTarget;
+
+    public struct Edge
+    {
+        public Vector3 v1;
+        public Vector3 v2;
+
+        public Edge(Vector3 v1, Vector3 v2)
+        {
+          
+           this.v1 = v2;
+           this.v2 = v1;
+        
+        }
+    }
+
+
+    [MenuItem("Tools/Create Wiremesh")]
+    static void CreateWire() 
+    {
+        GenerateWiremesh window = EditorWindow.GetWindow<GenerateWiremesh>("创建Wiremesh");
+        window.minSize = new Vector2(300, 200);
+        window.Show();
+
+        
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("【第一步】指定网格对象", GUILayout.Width(150));
+        currentSelectTarget = (GameObject)EditorGUILayout.ObjectField("网格对象", currentSelectTarget, typeof(GameObject), true);
+
+        if (GUILayout.Button("处理"))
+        {
+            OnCreate();
+        }
+    }
+
+    void OnCreate()
+    {
+        //Path of the file
+        string path = Application.dataPath+ "/" + currentSelectTarget.name+ ".wire";
+        //Create File
+         File.WriteAllText(path, "WIREMESH\n");
+
+        //Read mesh data
+        //Content of the file
+        //string content = "0.5 -0.5 0.0 0.5 0.5 0.0" + "\n";
+        string content = ReadMesh();
+
+        //Add some to text to it
+        File.AppendAllText(path, content);
+    }
+
+    string ReadMesh()
+    {
+        Mesh mesh = currentSelectTarget.GetComponent<SkinnedMeshRenderer>().sharedMesh;
+        GetMeshEdges(mesh);
+        Edge[] edges = GetMeshEdges(mesh);
+        string wireMeshData="";
+        for (int i = 0; i < edges.Length; i++)
+        {
+            wireMeshData += VectorToString(edges[i].v1) + " " + VectorToString(edges[i].v2) + "\n";
+            //Debug.Log(i + ": " + VectorToString(edges[i].v1) + ", " + VectorToString(edges[i].v2));
+        }
+        return wireMeshData;
+    }
+
+    string VectorToString(Vector3 vector)
+    {
+        string vectorString = string.Format("{0} {1} {2}", vector.x, vector.y, vector.z);
+        return vectorString;
+    }
+    /*
+
+    void GetVertices()
+    {
+        Mesh newMesh = new Mesh();
+
+        Vector3[] vertices = mesh.vertices;
+        int[] triangles = mesh.triangles;
+        int triCount = triangles.Length;
+
+        Vector3[] newVertices = new Vector3[triCount];
+        for (int i = 0, imax = triCount; i < imax; i++)
+        {
+            newVertices[i] = vertices[triangles[i]];
+        }
+        newMesh.vertices = newVertices;
+    }
+    */
+
+
+    //三角面三条线条，统计总和，并没有去除重复的线条
+    private Edge[] GetMeshEdges(Mesh mesh)
+    {
+        List<Edge> edges = new List<Edge>();
+
+        for (int i = 0; i < mesh.triangles.Length; i += 3)
+        {
+            Vector3 v1 = mesh.vertices[mesh.triangles[i]];
+            Vector3 v2 = mesh.vertices[mesh.triangles[i + 1]];
+            Vector3 v3 = mesh.vertices[mesh.triangles[i + 2]];
+            edges.Add(new Edge(v1, v2));
+            edges.Add(new Edge(v1, v3));
+            edges.Add(new Edge(v2, v3));
+        }
+
+        return edges.ToArray();
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# VFX wiremesh
+A tool to generate wire for Unity visual effects graph mesh
+
+
+## Overview
+
+This project contains a wiremesh generator and a vfx to show wiremesh 
+
+## Software
+
+This project was created using Unity 2021.3.4f1c1 and high-definition Pipeline 12.1.7
+
+
